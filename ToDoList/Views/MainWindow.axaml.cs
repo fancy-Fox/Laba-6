f57eb7c1e6822e1fb04c509bd954d44e4377120f
gulay <gulay@DESKTOP-UT00N47>
2022-03-24 23:14:10 +0700

    Initial commit

---
+++ ToDoList/Views/MainWindow.axaml.cs
@@ -0,0 +1,17 @@
+using Avalonia.Controls;
+using Avalonia;
+using System;
+using ToDoList.Models;
+using ToDoList.ViewModels;
+using System.Linq;
+
+namespace ToDoList.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
